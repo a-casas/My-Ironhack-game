65e208a45d5c69608fa0d629f0f62ecb7bddd12f
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-11-01 20:51:40 +0000

    class/constructor issues resolved

--- scripts/objects.js
+++ scripts/objects.js
@@ -87,32 +87,24 @@
 
 
 
-class ObjectTest {
-	constructor(){
-		this.spriteWidth = 180
-		this.spriteHeight = 252
-		this.width = this.spriteWidth/2
-		this.height = this.spriteHeight/2
-		this.x = 300
-		this.y = 280
-		this.moving = false
-		this.frameX = 0
-		this.frameY = 0
-		// this.speed = 5
+class GameObject {
+	constructor(x, y, width, height, name, picture){
+		this.x = x
+		this.y = y
+		this.width = width
+		this.height = height
+		this.name = name
 		this.image = new Image()
-		this.image.src = "../images/vase.png"
-
+		this.image.src = picture
 	}
-
-	draw(){  //Zoom based on y position
-		ctx2.fillStyle = 'red'
-		ctx2.fillRect(this.x, this.y, this.width, this.height) //to test sprite alignment VS rectangle alignment
-		ctx2.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height )
+	draw(){
+		ctx2.fillStyle = 'blue'
+		ctx2.fillRect(this.x, this.y, this.width, this.height, this.name, this.image, this.picture)
+		ctx2.drawImage(this.image, this.x, this.y, this.width, this.height)
 	}
 }
-
-const objectTest = new ObjectTest()
-
+const objectTest = new GameObject(222, 222, 100, 100, "vase", "../images/vase.png")
+const objectTest2 = new GameObject(130, 225, 100, 100, "vase2", "../images/vase.png")
 
 
 
